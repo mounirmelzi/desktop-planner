73e9ef497bbd437c48503602204da8e3ddbfe954
Mounir <lm_melzi@esi.dz>
2023-04-29 11:45:01 +0200

    add MVC Folders Structure

--- src/main/java/com/example/controllers/HelloController.java
+++ src/main/java/com/example/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.planner;
+package com.example.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module com.example.planner {
 
     opens com.example.planner to javafx.fxml;
     exports com.example.planner;
+    exports com.example.controllers;
+    exports com.example.core;
+    opens com.example.controllers to javafx.fxml;
 }
