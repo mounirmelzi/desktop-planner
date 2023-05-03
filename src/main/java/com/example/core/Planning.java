75f5bd9e2a786397a72d265e61f3a149c47204c7
Mounir <lm_melzi@esi.dz>
2023-05-03 10:14:26 +0200

    Améliorer la comparaison des taches

--- src/main/java/com/example/core/Planning.java
+++ src/main/java/com/example/core/Planning.java
@@ -1,13 +1,12 @@
 package com.example.core;
 
 import java.io.Serializable;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.TreeSet;
 
 public class Planning implements Serializable {
     private final TreeSet<Day> days = new TreeSet<>();
-    private final TreeSet<Tache> taches = new TreeSet<>(Comparator.reverseOrder()); // les taches no programmées : unscheduled (l'ensemble des taches)
+    private final TreeSet<Tache> taches = new TreeSet<>(); // les taches no programmées : unscheduled (l'ensemble des taches)
     private final HashSet<Project> projects = new HashSet<>();
     private Periode periode;
 }
--- src/main/java/com/example/core/Tache.java
+++ src/main/java/com/example/core/Tache.java
@@ -4,24 +4,26 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
 import java.time.Duration;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Comparator;
 
 public abstract class Tache implements Comparable<Tache>, Serializable {
     protected String nom;
     protected Duration duree;
     protected Priority priority;
-    protected LocalDate deadline;
+    protected LocalDateTime deadline;
     protected Category category;
     protected State state;
 
     @Override
-    public int compareTo(@NotNull Tache o) {
-        int cmp = priority.compareTo(o.priority);
-        if (cmp != 0) {
-            return cmp;
-        }
+    public int compareTo(@NotNull Tache other) {
+        Comparator<Tache> priorityComparator = new PriorityComparator();
+        Comparator<Tache> deadlineComparator = new DeadlineComparator();
 
-        return deadline.compareTo(o.deadline);
+        return priorityComparator.reversed()
+                .thenComparing(deadlineComparator)
+                .thenComparing((tache1, tache2) -> tache1.nom.compareTo(tache2.nom))
+                .compare(this, other);
     }
 
     @Override
@@ -34,5 +36,20 @@ public abstract class Tache implements Comparable<Tache>, Serializable {
                 ", category=" + category +
                 ", state=" + state +
                 '}';
+    }
+
+
+    public static class PriorityComparator implements Comparator<Tache> {
+        @Override
+        public int compare(Tache t1, Tache t2) {
+            return t1.priority.compareTo(t2.priority);
+        }
+    }
+
+    public static class DeadlineComparator implements Comparator<Tache> {
+        @Override
+        public int compare(Tache t1, Tache t2) {
+            return t1.deadline.compareTo(t2.deadline);
+        }
     }
 }
