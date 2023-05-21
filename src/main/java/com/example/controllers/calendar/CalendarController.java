b3c721026b1e3a93ea2c0d51ec6d6b12876226d9
Mounir <lm_melzi@esi.dz>
2023-05-21 21:47:15 +0200

    calendar ui update

--- src/main/java/com/example/controllers/calendar/CalendarController.java
+++ src/main/java/com/example/controllers/calendar/CalendarController.java
@@ -15,12 +15,15 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class CalendarController extends Controller implements Initializable {
@@ -32,6 +35,8 @@ public class CalendarController extends Controller implements Initializable {
     private GridPane calendarGridPane;
     @FXML
     private AnchorPane dayInfoAnchorPane;
+    @FXML
+    private HBox daysNamesContainer;
 
     private final User user;
     private YearMonth currentYearMonth;
@@ -68,13 +73,23 @@ public class CalendarController extends Controller implements Initializable {
         monthLabel.setText(currentYearMonth.getMonth().toString());
         yearLabel.setText(String.valueOf(currentYearMonth.getYear()));
         calendarGridPane.getChildren().clear();
+        daysNamesContainer.getChildren().clear();
+
+        for (int i = 1; i <= 7; i++) {
+            Label dayNameLabel = new Label();
+            dayNameLabel.setText(LocalDate.of(currentYearMonth.getYear(), currentYearMonth.getMonth(), i).getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.getDefault()).toUpperCase());
+            dayNameLabel.setPrefSize(50, 50);
+            dayNameLabel.getStyleClass().add("day-label");
+            dayNameLabel.setStyle("-fx-border-color: #123456; -fx-background-color: #123456; -fx-text-fill: #f0f0f0;");
+            daysNamesContainer.getChildren().add(dayNameLabel);
+        }
 
         int daysInMonth = currentYearMonth.lengthOfMonth();
         for (int i = 1; i <= daysInMonth; i++) {
             LocalDate date = LocalDate.of(currentYearMonth.getYear(), currentYearMonth.getMonth(), i);
             DayLabel dayLabel = new DayLabel(date);
 
-            calendarGridPane.add(dayLabel, ((i < 29) ? ((i - 1) % 7) : (2 + (i - 1) % 7)), (i + 6) / 7);
+            calendarGridPane.add(dayLabel, (i - 1) % 7, (i + 6) / 7);
         }
     }
 
@@ -94,20 +109,30 @@ public class CalendarController extends Controller implements Initializable {
 
     private class DayLabel extends Label {
         private final Day day;
+        private final LocalDate date;
 
         public DayLabel(@NotNull LocalDate date) {
             super(String.valueOf(date.getDayOfMonth()));
             this.day = user.getCalendrier().getDayByDate(date);
+            this.date = date;
 
             this.setOnMouseClicked(event -> handleDayLabelMouseClicked(event, day, date));
             this.setPrefSize(50, 50);
-            this.setStyles(day != null && day.hasCreneaux());
+            this.setStyles();
             this.setCursor(Cursor.HAND);
         }
 
-        private void setStyles(boolean mark) {
+        private void setStyles() {
             this.getStyleClass().add("day-label");
-            this.setStyle((mark ? "-fx-border-color: #123456;" : ""));
+
+            if (day != null && day.hasCreneauxOccupees()) {
+                this.getStyleClass().clear();
+                this.getStyleClass().add("day-label-occupe");
+            } else if (day != null && day.hasCreneaux())
+                this.setStyle("-fx-border-color: #123456;");
+
+            if (date.isEqual(LocalDate.now()))
+                this.setStyle("-fx-border-color: #ee6c4d;");
         }
     }
 }
